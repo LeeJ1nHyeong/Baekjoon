f1334dc23342ffba535572da9610df4c5280567b
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-04-08 12:02:10 +0900

    240408 우선순위 큐 (python)

---
+++ SILVER/S2_2075_N번째 큰 수.py
@@ -0,0 +1,19 @@
+import heapq
+
+n = int(input())
+heap = []  # 우선순위 큐를 구현할 heap
+
+for _ in range(n):
+    number = list(map(int, input().split()))
+    for num in number:
+        # 메모리 제한으로 인해 heap의 길이를 n으로 제한
+        if len(heap) < n:
+            heapq.heappush(heap, num)
+        else:
+            # heap이 다 찼을 경우 현재 숫자가 heap의 최솟값보다 크다면 최솟값을 pop하고 그 숫자를 push
+            if num > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
+
+heap.sort()  # 오름차순 정렬
+print(heap[0])  # heap 내의 최솟값(N번째 큰 수) 출력
