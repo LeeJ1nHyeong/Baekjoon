7f6e556a8e8d62e77f14ed8771d191b8568dd29d
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-11-17 11:44:38 +0900

    241117 BFS (python)

---
+++ SILVER/S2_24445_알고리즘 수업 - 너비 우선 탐색 2.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+tree = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    tree[u].append(v)
+    tree[v].append(u)
+
+# 각 노드 별로 연결되어있는 다음 노드들을 내림차순으로 정렬
+for t in tree:
+    t.sort(reverse=True)
+
+queue = deque()
+queue.append(r)
+visited[r] = 1
+
+turn = 1
+while queue:
+    now = queue.popleft()
+
+    for node in tree[now]:
+        if visited[node]:
+            continue
+
+        turn += 1
+        visited[node] = turn
+        queue.append(node)
+
+for i in range(1, n + 1):
+    print(visited[i])
