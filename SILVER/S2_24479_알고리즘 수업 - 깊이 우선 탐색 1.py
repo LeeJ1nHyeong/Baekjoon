8e59cf7eae8ca178e975e42e1fdd376b8f39f34a
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-11-20 19:08:46 +0900

    241120 DFS (python)

---
+++ SILVER/S2_24479_알고리즘 수업 - 깊이 우선 탐색 1.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)  # 재귀 제한 설정
+
+
+def dfs(now):
+    global turn
+    visited[now] = turn
+    turn += 1
+
+    for node in tree[now]:
+        if not visited[node]:
+            dfs(node)
+
+
+n, m, r = map(int, input().split())
+tree = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    tree[u].append(v)
+    tree[v].append(u)
+
+# 현재 노드의 다음 노드를 오름차순으로 정렬
+for t in tree:
+    t.sort()
+
+turn = 1
+dfs(r)  # 시작 노드부터 dfs 탐색 진행
+
+for i in range(1, n + 1):
+    print(visited[i])
