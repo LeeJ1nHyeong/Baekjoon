261f827d4128d118eb0701003560eb86577f49a6
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-11-23 07:25:01 +0900

    241123 DFS (python)

---
+++ SILVER/S2_24482_알고리즘 수업 - 깊이 우선 탐색 4.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)  # 재귀 제한 설정
+
+
+def dfs(now, d):
+    # 탐색 노드의 깊이 저장
+    depth[now] = d
+
+    # 미방문 노드를 탐색하여 dfs 진행
+    for node in tree[now]:
+        if depth[node] == -1:
+            dfs(node, d + 1)
+
+
+n, m, r = map(int, input().split())
+tree = [[] for _ in range(n + 1)]
+depth = [-1] * (n + 1)
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    tree[u].append(v)
+    tree[v].append(u)
+
+# 인접 노드 내림차순 정렬
+for t in tree:
+    t.sort(reverse=True)
+
+dfs(r, 0)  # 루트 노드부터 dfs 탐색 진행
+
+for i in range(1, n + 1):
+    print(depth[i])
