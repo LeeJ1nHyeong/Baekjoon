cf6e5dfe2c920dbd45c961493b132936c3de5e7b
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-12-22 20:59:15 +0900

    241222 백트래킹, 조합 (python)

---
+++ SILVER/S4_5568_카드 놓기.py
@@ -0,0 +1,34 @@
+n = int(input())
+k = int(input())
+numbers = [int(input()) for _ in range(n)]
+new_numbers = set()
+
+
+# 백트래킹 (4716 ms)
+def backtrack(cnt, num):
+    if cnt == k:
+        new_numbers.add(int(num))
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = 1
+            backtrack(cnt + 1, num + str(numbers[i]))
+            visited[i] = 0
+
+visited = [0] * n
+
+for i in range(n):
+    visited[i] = 1
+    backtrack(1, str(numbers[i]))
+    visited[i] = 0
+
+print(len(new_numbers))
+
+
+# 라이브러리 itertool 이용 (44 ms)
+import itertools
+
+for i in list(itertools.permutations(numbers, k)):
+    new_numbers.add("".join(map(str, i)))
+
+print(len(new_numbers))
