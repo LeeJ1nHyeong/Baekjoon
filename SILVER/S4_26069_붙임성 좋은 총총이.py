1d6fb5c7f18e60c6cc0629f581cd7deac533ecf8
LeeJ1nHyeong <wlsgud4084@naver.com>
2025-03-06 18:49:29 +0900

    250306 해시 (python)

---
+++ SILVER/S4_26069_붙임성 좋은 총총이.py
@@ -0,0 +1,15 @@
+n = int(input())
+dance = set()
+
+for _ in range(n):
+    a, b = input().split()
+
+    if a == "ChongChong" or b == "ChongChong":
+        dance.add(a)
+        dance.add(b)
+
+    if a in dance or b in dance:
+        dance.add(a)
+        dance.add(b)
+
+print(len(dance))
